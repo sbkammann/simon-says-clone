85b201a0851e264ce35ea5eeb72d9ee6415de73e
Shannan Kammann <sbkammann@gmail.com>
2018-12-02 17:40:25 -0800

    feat: Added promise that ensures lightUp resolves before next is invoked

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@ const blue = document.getElementById('blue');
 const green = document.getElementById('green');
 const red = document.getElementById('red');
 const yellow = document.getElementById('yellow');
-
+const queue = [1, 2, 3, 4]; //stores the sequence in which the tiles will light up
+let roundNum = 1; // counts the number of rounds
 
 // play sequence and store in array random number generator
 //random number between 1 and 4 may need to improve so there are fewer same numbers in sequence
-let randNum;
-for (let i=0; i<20; i++){
-  randNum = Math.floor((Math.random() * 4) + 1);
-  console.log(randNum);
-}
+//add random number to queue
+// function addToQueue(){
+//    queue[roundNum-1] = Math.floor((Math.random() * 4) + 1);
+// }
+
 //simulate square lighting up
 function lightUp(num){
-
   switch(num) {
     case 1:
         blue.style.backgroundColor = '#00FFFF'; // cyan
@@ -30,10 +30,10 @@ function lightUp(num){
         }, 750); // 3/4 of a second
         break;
     case 3:
-        red.style.backgroundColor=   '#FF00CC'; // Hot Magenta
+        red.style.backgroundColor=   '#FFB6C1'; // Hot pink
         //returns back to original color
         setTimeout(function(){
-          red.style.backgroundColor = 'red';
+          red.style.backgroundColor = '#B22222';
         }, 750); // 3/4 of a second
         break;
     case 4:
@@ -44,10 +44,21 @@ function lightUp(num){
         }, 750); // 3/4 of a second
         break;
     }
-
-  // need to create delay with timer set?
 }
+//ensures the lightUp function finishes before the next is invoked
+(function fn(i) {
+    if (i < queue.length) new Promise((resolve, reject) => {
+        setTimeout( () => {
+            lightUp(queue[i]);
+            resolve();
+        }, 1000);
+    }).then(fn.bind(null, i+1));
+})(0);
 
 
+
+
+//might have to use generator
+//add delay before last light up
 //event listerners check to see if the same as stored generator, counter so computer knows when to start again
 //timer, countdown, score, round, sound, reset/start button
